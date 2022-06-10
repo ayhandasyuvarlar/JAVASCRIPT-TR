45083c66f500c36d66ae1f0df801986553dfd00f
Ayhan Dasyuvarlar <ayhandasyuvarlar@gmail.com>
2022-06-10 15:17:23 +0300

    post request

--- Subject-30/app.js
+++ Subject-30/app.js
@@ -8,14 +8,38 @@ class Request {
         .catch((err) => reject(err))
     })
   }
+  //post request
+  post(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => resolve(data))
+        .catch((err) => reject(err))
+    })
+  }
 }
 
 const request = new Request()
-let albums;
-request
-  .get('https://jsonplaceholder.typicode.com/albums/')
+request.get('https://jsonplaceholder.typicode.com/albums/')
   .then((albums) => {
     console.log(albums)
   })
   .catch((err) => console.log(err))
 
+  
+request.post('https://jsonplaceholder.typicode.com/posts', {
+  userID: 1,
+  title: 'hello',
+})
+.then(newAlbum =>{
+    console.log(newAlbum)
+})
+.catch(err =>{
+    console.log(err)
+})
