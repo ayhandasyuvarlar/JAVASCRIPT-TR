62158f02d9d50251b0a8224fecaef270c9a1abf4
Ayhan Dasyuvarlar <ayhandasyuvarlar@gmail.com>
2022-06-10 11:01:52 +0300

    promise object and then, catch , errormessage

--- Subject-28/app.js
+++ Subject-28/app.js
@@ -1,13 +1,45 @@
-function getData(data){
-    return new Promise(function(resolve,reject){
-       setTimeout(function(){
-        reject("Olumsuz sonuç")
-       },5000)
+// function getData(data){
+//     return new Promise(function(resolve,reject){
+//        setTimeout(function(){
+//          if(typeof data === "string"){
+//           resolve(data)
+//          }else{
+//            reject( new Error("Please , string enter a value"))
+//          }
+//        },5000)
 
-    })
-} // promise objesi dönüştüren  fonksiyon
+//     })
+// } // promise objesi dönüştüren  fonksiyon
  
 
 
+// getData(24)
+// .then(response =>{ console.log(response)})
+// .catch(err =>{console.error(err)})
 
-console.log(getData("merhaba"))
+
+
+function addTo(number){
+    return new Promise((resolve, reject) =>{
+        setTimeout( ()=>{
+              if(typeof number === "number"){
+                  resolve(number + 2)
+              }
+              else(
+                  reject(new Error("Please , number enter a value"))
+              )
+        },3000)
+    })
+}
+
+addTo(25)
+.then(response =>{
+    console.log(response)
+    return response *3*3;
+})
+.then(response2 =>{
+    console.log(response2)
+})
+.catch(err => {
+    console.error(err)
+})
